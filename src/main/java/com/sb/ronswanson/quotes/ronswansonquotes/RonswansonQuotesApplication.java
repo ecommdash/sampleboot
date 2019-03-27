6dae2727eff10bc9a99b94cf40abe8b14627ef66
Sharath Ballari <shaballa@publicisgroupe.net>
2019-03-27 15:19:07 +0530

    Updated the config

--- src/main/java/com/sb/ronswanson/quotes/ronswansonquotes/RonswansonQuotesApplication.java
+++ src/main/java/com/sb/ronswanson/quotes/ronswansonquotes/RonswansonQuotesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class RonswansonQuotesApplication {
 
 	public static void main(String[] args) {
